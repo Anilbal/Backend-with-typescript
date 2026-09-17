87532c5e5b7f9c5cf0adbe27f3264bcbb676e7b8
Anil Tamang <anilbal7676@gmail.com>
2026-09-17 22:38:45 +0545

    port change

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is running!");
